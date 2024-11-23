fee18f573ec6ef660b17324b13ac7a62e29807e5
Stephan Lensky <stephanl.public@gmail.com>
2024-11-23 13:57:05 -0500

    Exit after command completes

--- entrypoint_user.sh
+++ entrypoint_user.sh
@@ -91,4 +91,9 @@ cleanup() {
 trap cleanup TERM INT
 
 wait $command_pid
+echo "Command completed."
+echo "Stopping Wayland session..."
+kill $sway_pid 2>/dev/null
+while ps -p $sway_pid > /dev/null 2>&1; do sleep 1; done
+echo "Done."
 wait $sway_pid
